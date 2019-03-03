11e6f5bc9885bafe535472e564ca5ac6417edf3b
Vincent den Boer <vincent@shishkabab.net>
2019-03-03 16:11:03 +0100

    Update package.json and add forgotten file

---
+++ ts/index.tests.ts
@@ -0,0 +1,10 @@
+import * as expect from 'expect'
+import * as graphql from "graphql";
+
+export function expectGraphQLSchemaToEqual(schema : graphql.GraphQLSchema, expected : string) {
+    const expectLines = expected.split('\n')
+    const secondExpectLine = expectLines[1]
+    const leadingSpaces = /^\s+/.exec(secondExpectLine)[0]
+    const expectWithoutLeadingSpaces = expectLines.slice(1, -1).map(line => line.slice(leadingSpaces.length)).join('\n')
+    expect(graphql.printSchema(schema).trim()).toEqual(expectWithoutLeadingSpaces)
+}
